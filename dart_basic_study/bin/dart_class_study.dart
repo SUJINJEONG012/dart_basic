d429e965a741ce5499ea8cfba75e5b04ebe85c1b
JEONGSUJIN012 <peekaboo32@naver.com>
2022-04-13 21:19:27 +0900

    생성자함수

--- dart_basic_study/bin/dart_class_study.dart
+++ dart_basic_study/bin/dart_class_study.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 void main() {
   print('class 공부');
-  Student girlStudent = Student(); //인스턴스 생성
+  Student girlStudent = Student('송혜교', 30); //인스턴스 생성
   print(girlStudent.name);
   print(girlStudent.age);
 
@@ -15,8 +15,14 @@ void main() {
 }
 
 class Student {
+  // Student(this.name, this.age);
+  Student(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
   String name = 'sujin';
   int? age;
+  //변수2개 생성
 
   void printInfo() {
     print('-------');
@@ -24,4 +30,6 @@ class Student {
     print('age: $age');
     print('-------');
   }
+
+  //함수1개 생성
 }
