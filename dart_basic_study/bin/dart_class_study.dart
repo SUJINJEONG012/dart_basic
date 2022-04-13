bea47286d959421049ec54259a17117564fd6341
JEONGSUJIN012 <peekaboo32@naver.com>
2022-04-13 20:56:56 +0900

    클래스

--- dart_basic_study/bin/dart_class_study.dart
+++ dart_basic_study/bin/dart_class_study.dart
@@ -1,3 +1,27 @@
+import 'dart:async';
+
 void main() {
   print('class 공부');
+  Student girlStudent = Student(); //인스턴스 생성
+  print(girlStudent.name);
+  print(girlStudent.age);
+
+  girlStudent.name = '수진';
+  girlStudent.age = 24;
+
+  print(girlStudent.name);
+  print(girlStudent.age);
+  girlStudent.printInfo();
+}
+
+class Student {
+  String name = 'sujin';
+  int? age;
+
+  void printInfo() {
+    print('-------');
+    print('name: $name');
+    print('age: $age');
+    print('-------');
+  }
 }
